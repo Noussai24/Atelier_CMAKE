993d24b466bfde47c7d85b7953218bfbf0036b84
Noussaiba <166798784+Noussai24@users.noreply.github.com>
2024-05-23 11:47:14 +0200

    Update main.c
    ok

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ int main() {
     int result = car(5);
     printf("The square of 5 is: %d\n", result);
 
-    result = divide(10, 2); // Renommer l'appel à la fonction div en divide
+    result = divide(10, 2); // Renommé l'appel à la fonction div en divide
     printf("10 divided by 2 is: %d\n", result);
 
     return 0;
