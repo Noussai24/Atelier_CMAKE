ee62a6f35417f7adf1ef3032b97b33aeabb8e7a5
Noussaiba <166798784+Noussai24@users.noreply.github.com>
2024-05-23 11:34:34 +0200

    Update main.c
    correction

--- src/main.c
+++ src/main.c
@@ -1,42 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "./../include/calculator.h"
 #include "calculator.h"
-
-
-int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
-}
 
 int main() {
     int result = car(5);
     printf("The square of 5 is: %d\n", result);
+
+    result = divide(10, 2); // Renommer l'appel à la fonction div en divide
+    printf("10 divided by 2 is: %d\n", result);
+
     return 0;
 }
-		
